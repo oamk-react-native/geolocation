c99a71c0a5606cf88f8217daf090b8c673c92539
Jouni Juntunen <jouni.juntunen@oamk.fi>
2021-09-29 09:41:46 +0300

    Finished and updated autumn 2021 example.

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const reply = await Location.requestForegroundPermissionsAsync();
+      if (reply.granted) {
+        const location = await Location.getCurrentPositionAsync({});
+        setLatitude(location.coords.latitude);
+        setLongitude(location.coords.longitude);
+        setIsLoading(false);
+      }
+    })();
+  }, [])
+
+  if (isLoading) {
+    return <View style={styles.container}><Text>Retrieving location...</Text></View>
+  }
+  else {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <View style={styles.container}>
+        <Text>Location: </Text>
+        <Text>{latitude.toFixed(3)}</Text>
+        <Text>{longitude.toFixed(3)}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
